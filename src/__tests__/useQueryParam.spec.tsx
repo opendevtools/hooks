71b00257f913a0fdb34c371f00b8c22f3d65a341
Rickard Laurin <rickard.laurin@gmail.com>
2019-01-16 08:30:45 +0100

    test: add test for query param with string array

--- src/__tests__/useQueryParam.spec.tsx
+++ src/__tests__/useQueryParam.spec.tsx
@@ -8,7 +8,11 @@ afterEach(cleanup)
 function NeedsParams({ param }: { param: string }) {
   const queryParam = useQueryParam(param)
 
-  return <div>{queryParam}</div>
+  return (
+    <div>
+      {typeof queryParam === 'string' ? queryParam : queryParam.join(',')}
+    </div>
+  )
 }
 
 test('gets query param from window location search', () => {
@@ -17,4 +21,15 @@ test('gets query param from window location search', () => {
   const { getByText } = render(<NeedsParams param="muppet" />)
 
   expect(getByText(/cookiemonster/i)).toBeInTheDocument()
+})
+
+test('handles query params with string array', () => {
+  jsdom.reconfigure({
+    url: 'http://test.com/?muppets=kermit&muppets=cookiemonster',
+  })
+
+  const { getByText } = render(<NeedsParams param="muppets" />)
+
+  expect(getByText(/cookiemonster/i)).toBeInTheDocument()
+  expect(getByText(/kermit/i)).toBeInTheDocument()
 })
