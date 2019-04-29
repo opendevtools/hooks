807471cfc35759267d0385ad4955b1629a0d9cc4
Rickard Laurin <rickard.laurin@gmail.com>
2019-04-29 08:17:48 +0200

    fix: export useQueryParams

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './useToggle'
 export * from './useQueryParam'
+export * from './useQueryParams'
 export * from './useDebounce'
 export * from './useLocalStorage'
 export * from './useStorage'
